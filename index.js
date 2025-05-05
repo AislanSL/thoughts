5995a6d4e00c532b97b084303c0949e1c2e6d740
AislanSL <aislansiqueira2001@gmail.com>
2025-05-05 15:43:04 -0300

    finalizando estrutura

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
+// IMPORTAÇÕES
 const express = require('express')
-const exphbs = require('express-handlebars')
+const exphbs = require ('express-handlebars')
 const session  = require('express-session')
 const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
 const conn = require('./db/conn')
 
 const app = express()
+
+// TEMPLATE ENGINE
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+// RECEBE RESPOSTA DO BODY
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+// SESSION MIDDLEWARE
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function () {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    }),
+)
+
+// FLASH MESSAGES
+app.use(flash())
+
+// PUBLIC PATH
+app.use(express.static('public'))
+
+// SET SESSION TO RES
+app.use((req, res, next) => {
+    if(req.session.userid) {
+        res.locals.session = req.session
+    }
+
+    next()
+})
 
 conn
     .sync()
