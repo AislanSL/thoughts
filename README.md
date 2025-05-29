c0d3a8e6e7b2a3b2700d33ee1315b8e4c7ac2a5e
AislanSL <aislansiqueira2001@gmail.com>
2025-05-29 14:35:02 -0300

    adiciona readme e container do bd

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# thoughts
+# 🧠 Thoughts App
+
+Aplicação Node.js com padrão MVC e template engine Handlebars, conectada a um banco de dados MySQL via Sequelize. O banco roda em um container Docker.
+
+---
+
+## 🚀 Tecnologias utilizadas
+
+- Node.js
+- Express
+- Sequelize
+- Handlebars
+- MySQL (via Docker)
+- Docker + Docker Compose
+
+---
+
+## 📦 Pré-requisitos
+
+- [Node.js](https://nodejs.org/)
+- [Docker](https://www.docker.com/)
+- [Docker Compose](https://docs.docker.com/compose/)
+
+---
+
+## ⚙️ Configuração do ambiente
+
+### 1. Clone o projeto
+
+```bash
+git clone https://github.com/seu-usuario/nome-do-repo.git
+cd nome-do-repo
--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,26 @@
+require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('thoughts', 'root', 'Abc@123456', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT
+    }
+)
 
-try {
-    sequelize.authenticate()
-    console.log('Conectado com sucesso')
-} catch(err) {
-    console.log(`Não foi possível conectar: ${err}`)
+async function connect() {
+    try {
+        await sequelize.authenticate()
+        console.log('Conectado com sucesso')
+    } catch (err) {
+        console.log(`Não foi possível conectar: ${err}`)
+    }
 }
+
+connect()
 
 module.exports = sequelize
